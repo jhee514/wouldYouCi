4dd8494452c68a8402183aae8e4558c4d9ea464e
jisunlee <jisunl0423@gmail.com>
2020-05-12 10:48:11 +0900

    Refactor wouldyouci install vuex, vuetify, vue-router Refs #S02P31A406-19
    Former-commit-id: 387889454da3f24453f5e785f6af8e2fe6530840

---
+++ wouldyouci/src/components/nav/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='nav'>
+    nav
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style src="./Nav.css" scoped></style>
---
+++ wouldyouci/src/components/signup/Signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='signup'>
+    signup
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup'
+}
+</script>
+
+<style src="./Signup.css" scoped></style>
