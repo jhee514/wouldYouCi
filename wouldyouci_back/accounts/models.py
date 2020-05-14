1ac054562d48bb044112eab43d0015b6d904877f
조현동 <jay.hyundong@gmail.com>
2020-05-14 17:22:51 +0900

    Add accounts app User model and back/.gitignore
    Former-commit-id: 759b6b5459516d86fb6bcc65c790c756934aea49

--- wouldyouci_back/accounts/models.py
+++ wouldyouci_back/accounts/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, email, get_agreement, password=None):
+        if not username:
+            raise ValueError('Users must have an username')
+
+        user = self.model(
+            username=username,
+            email=email,
+            get_agreement=get_agreement,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            username,
+            email=email,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    get_agreement = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'get_agreement']
+
+    def __str__(self):
+        return '%s' % self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
