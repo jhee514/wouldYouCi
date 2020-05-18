b54ce3da733e8d6accc37d9f558351d6a5e3e6d5
이지선 <jisunl0423@gmail.com>
2020-05-18 17:47:04 +0900

    Refactor UserPage Refs #S02P31A406-36 Add MovieList && Add SettingCard && Add UserInfo in UserPage
    Former-commit-id: ce2e34902d4384f2084090b92fbfc49069a48bfc

---
+++ wouldyouci/src/components/userPage/userInfo/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="userInfo">
+    <img class="userImage" @click="isShow = !isShow" src="../../../assets/basicUserImage.png" />
+    <v-dialog v-model="isShow">
+      <v-row justify="center">
+        <img class="bigUserImage" src="../../../assets/basicUserImage.png">
+      </v-row>
+    </v-dialog>
+    <span class="userName">userName</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  data() {
+    return {
+      isShow: false
+    }
+  }
+}
+</script>
+
+<style src="./UserInfo.css" scoped></style>
