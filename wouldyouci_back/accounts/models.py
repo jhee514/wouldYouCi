3e358755c941be6373f79402371e21dbbc1b2298
조현동 <jay.hyundong@gmail.com>
2020-05-18 15:24:05 +0900

    Add accounts model rating and sample data in fixtures/
    Former-commit-id: c5d0b49907ced2e3254056d6bd96f3db856fafb9

--- wouldyouci_back/accounts/models.py
+++ wouldyouci_back/accounts/models.py
@@ -61,7 +61,7 @@ class User(AbstractBaseUser):
 
 class Rating(models.Model):
     comment = models.TextField(blank=True, null=True)
-    rating = models.IntegerField()
+    score = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='ratings')  # movie.ratings
