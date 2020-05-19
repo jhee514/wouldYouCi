862cca9cb5c11fb13d5f88a30d79abd02bcb0588
Kim, Ji Hee <514kim@gmail.com>
2020-05-19 16:54:11 +0900

    Mod movieDetail add trailer
    Former-commit-id: b8161ca680de7fa46f2a4e61f1730e6c706dc6dc

--- wouldyouci/src/components/movieDetail/MovieDetail.vue
+++ wouldyouci/src/components/movieDetail/MovieDetail.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class='movieDetail'>
+<v-app>
+  <div>
     <Title />
-    MovieDetail
+    <div class="movieDetail">
+      <div class="header">
+        {{ movieTitle }}
+      </div>
+
+      <div class="movieTrailer">
+        <iframe 
+        title=movieTitle
+        width=auto 
+        height=auto 
+        :src=movieTrailerLink
+        allowfullscreen 
+        frameborder="0" 
+        scrolling="no" 
+        allow="autoplay"></iframe>
+      </div>
+      <div class="rating">
+        <v-rating
+          v-model="movieRating"
+          background-color="orange lighten-3"
+          color="orange"
+          half-increments
+          readonly="True"
+          dense
+        ></v-rating>
+        
+      </div>
+
+      <v-divider></v-divider>
+      <div class="movieInfo">
+
+    
+      </div>
+
+      <div class="movieReview">
+      </div>
+      
+    </div>
     <Nav />
   </div>
+</v-app>
 </template>
 
 <script>
@@ -14,6 +53,14 @@ export default {
   components: {
     Nav,
     Title,
+  },
+  data() {
+    return {
+      movieTitle: "패왕별희 디 오리지널",
+      movieDirector: "누굴까",
+      movieTrailerLink: "https://play-tv.kakao.com/embed/player/cliplink/408733684?service=daum_movie",
+      movieRating: 3.5,
+    }
   }
 }
 </script>
