6cba2baa8777e0ef42babeac5a7c621fc4c2255d
이지선 <jisunl0423@gmail.com>
2020-05-19 14:54:25 +0900

    Add ChangeUserImage Refs #S02P31A406-41 Add ChangeUserImage page layout
    Former-commit-id: 66ce8f4afec88a5e1d476ab530cd66863ce0d7d0

---
+++ wouldyouci/src/components/userPage/changeUserInfo/ChangeUserImage.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card class="changeUserImage">
+    <v-file-input
+      class="userImage"
+      label="프로필 사진"
+      prepend-icon="fas fa-camera"
+      @change="onFileChange"
+      ref="fileInput"
+    >
+    </v-file-input>
+    <v-img
+      class="prev"
+      :src="showImage"
+      small-chips accept="image/*"
+      clearable
+    >
+    </v-img>
+    <v-card-actions>
+      <v-item-group>
+        <v-btn text @click="closeDialog">저장</v-btn>
+        <v-btn text @click="closeDialog">취소</v-btn>
+      </v-item-group>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'changeUserImage',
+  data() {
+    return {
+      image: null
+    }
+  },
+  computed: {
+    showImage() {
+      if (this.image) {
+        const url = URL.createObjectURL(this.image)
+        return url
+      }
+      return require("../../../assets/stars.png")
+    }
+  },
+  methods: {
+    onFileChange(file) {
+      this.image = file;
+    },
+    closeDialog() {
+      this.$emit("changeUserImage", "close");
+    },
+  }
+}
+</script>
+
+<style src="./ChangeUserImage.css" scoped></style>
