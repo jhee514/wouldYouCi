e8cc1132ba11e2f71900332fd38059db328634d3
송원석 <dnjstjr0517@gmail.com>
2020-05-22 19:55:37 +0900

    Add cinema model
    Former-commit-id: 6452472bd211632356f701092b81ccc135bfaf58

--- wouldyouci_back/movies/models.py
+++ wouldyouci_back/movies/models.py
@@ -28,3 +28,28 @@ class Movie(models.Model):
     actors = models.ManyToManyField(People, related_name='movie_actors')
 
     # ratings = models.ManyToManyField(User, through='Rating')
+
+class Cinema(models.Model):
+    region = models.CharField(max_length=30)
+    area = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=20)
+    tel = models.CharField(max_length=30, blank=True, null=True)
+    address = models.CharField(max_length=50)
+    x = models.CharField(max_length=50)
+    y = models.CharField(max_length=50)
+    url = models.URLField(max_length=250)
+    public = models.TextField(blank=True, null=True)
+    parking = models.TextField(blank=True, null=True)
+    type = models.CharField(default='기타')
+    img = models.URLField(max_length=250, blank=True, null=True)
+
+class Onscreen(models.Model) :
+    # movie = models.ManyToManyField(Movie)
+    # cinema = models.ManyToManyField(Cinema)
+    date = models.DateField()
+    start_time : models.CharField(max_length=10)
+    end_time: models.CharField(max_length=10)
+    total_seats: models.CharField(max_length=5)
+    seats: models.CharField(max_length=5)
+    url : models.URLField(max_length=250, blank=True, null=True)
