e9af8a7d141390f8f25a8862c0b0b3e978698eae
조현동 <jay.hyundong@gmail.com>
2020-05-26 13:55:44 +0900

    Add api user movie rating C, U, D
    Former-commit-id: b4ca490f3aedff44c31295fcc6fec3c3773780c9

--- wouldyouci_back/accounts/urls.py
+++ wouldyouci_back/accounts/urls.py
@@ -4,9 +4,10 @@ from . import views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 # router.register(r'accounts/view/', views.AccountViewSet, basename='test')
-# router.register(r'admin', views.AdminUserViewSet)
+# router.register(r'movie/rating', views.RatingViewSet)
 
 urlpatterns = [
+    # path('', include(router.urls)),
     # path('', views.signup),
     path('login/', obtain_jwt_token),
     path('', views.AccountView.as_view()),
@@ -15,4 +16,8 @@ urlpatterns = [
     # path('rating/init/'),
     path('login/rating/', views.get_rating_tf, name='get_rating_tf'),
     # 로그인시 레이팅 페이지
+
+    # 영화 레이팅
+    path('movie/rating/', views.create_rating, name='create_rating'),
+    path('movie/rating/<int:rating_id>/', views.patch_delete_rating, name='path_delete_rating'),
 ]
