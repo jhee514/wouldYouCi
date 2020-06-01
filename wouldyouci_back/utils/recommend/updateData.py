11700890487a38eee226b964eb4409af378f82a2
송원석 <dnjstjr0517@gmail.com>
2020-06-01 18:16:19 +0900

    Mod updateData, Filtering
    Former-commit-id: 6b7ad1ed697a6034eac9c35866b8393b9cd5d39f [formerly 913d753b48974ae062a4bcfce7dc310b8d93528f]
    Former-commit-id: 4b14506bf8e5eefeb1523fc538e9572f4a85559d

--- wouldyouci_back/utils/recommend/updateData.py
+++ wouldyouci_back/utils/recommend/updateData.py
@@ -19,6 +19,42 @@ def get_train_data() :
     genres_dummies.to_pickle('./genre_train.p')
     conn.close()
 
+
+def get_train_data2() :
+    print("시작")
+    conn = pymysql.connect(host='15.164.96.65', port=3306, user='root', password='wouldyoucinema', db='wouldyouci')
+    sql = "SELECT * FROM wouldyouci.movies_movie_genres"
+    sql2 = "SELECT * FROM wouldyouci.movies_movie_directors"
+    sql3 = "SELECT * FROM wouldyouci.movies_movie_actors"
+    sql4 = "SELECT * FROM wouldyouci.movies_people"
+
+    movie_genres = pd.read_sql_query(sql, conn, index_col='movie_id')
+    movie_directors = pd.read_sql_query(sql2, conn, index_col='movie_id')
+    movie_actors = pd.read_sql_query(sql3, conn, index_col='movie_id')
+    people = pd.read_sql_query(sql4, conn, index_col='id')
+
+    genres = pd.read_csv('./genres.csv', index_col='id')
+
+    movie_people = pd.concat([movie_directors, movie_actors])
+
+    movie_genres = movie_genres.drop('id', axis='columns')
+    movie_people = movie_people.drop('id', axis='columns')
+
+    movie_genres['genre_id'] = movie_genres['genre_id'].apply(lambda x: genres.loc[x, 'name'] + "|")
+    movie_people['people_id'] = movie_people['people_id'].apply(lambda x: people.loc[x, 'name'] + "|")
+
+    movie_genres = movie_genres.groupby('movie_id').sum()
+    movie_people = movie_people.groupby('movie_id').sum()
+
+    genres_dummies = movie_genres['genre_id'].str.get_dummies(sep='|')
+    people_dummies = movie_people['people_id'].str.get_dummies(sep='|')
+
+    train = people_dummies.merge(genres_dummies, on='movie_id')
+
+    train.to_pickle('./movie_train.p')
+    print("끝")
+    conn.close()
+
 # 이건 처음 장르 테이블 가져오기 / 한번만 쓰고 더이상 안씀
 def get_genre_info() :
     conn = pymysql.connect(host='15.164.96.65', port=3306, user='root', password='wouldyoucinema', db='wouldyouci')
@@ -28,3 +64,6 @@ def get_genre_info() :
     result.to_csv(r'genres.csv', index=True)
 
     conn.close()
+
+
+get_train_data2()
