8980ed9eee8de85840e43c64049765598c1eb20a
JaehyunKim94 <didwjd169@gmail.com>
2020-06-07 11:37:39 +0900

    Fix match_code.py Add movie trailer based on youtube ref # s02p31a406-26
    Former-commit-id: ba5108dfeb76fde9d18c2880c9164b5518d7612d [formerly 3112255379a06e23eec021ece6dd7665de65f83a]
    Former-commit-id: 1e47a9315895c9d3c01fa6e4060374fe919b968a

--- wouldyouci_database/crawling/onetry.py
+++ wouldyouci_database/crawling/onetry.py
@@ -1,9 +1,9 @@
-import movie_on_ci
+# import movie_on_ci
 import match_code
-import movie_code_change
-import movie_on_today
+# import movie_code_change
+# import movie_on_today
 
-movie_on_today.getScreenInfo()
-movie_on_ci.getScreenInfo()
+# movie_on_today.getScreenInfo()
+# movie_on_ci.getScreenInfo()
 match_code.matchingMovieCode()
-movie_code_change.movieCodeChange()
+# movie_code_change.movieCodeChange()
